7b89c8d97c53ce94e3ea3388ced7e437843e9679
Oladele F <farayolaoladele@gmail.com>
2021-07-19 22:29:18 -0400

    Trying To Make Assets Load From Vapor CDN In Production

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,21 @@ mix
   .setPublicPath('dist')
   .js('resources/js/field.js', 'js')
   .sass('resources/sass/field.scss', 'css')
+
+if (mix.inProduction()) {
+  const ASSET_URL = process.env.ASSET_URL + "/";
+
+    mix.webpackConfig(webpack => {
+        return {
+            plugins: [
+                new webpack.DefinePlugin({
+                    "process.env.ASSET_PATH": JSON.stringify(ASSET_URL)
+                })
+            ],
+            output: {
+                publicPath: ASSET_URL
+            }
+        };
+    });
+  mix.version()
+}
