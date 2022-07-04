eb30e822e740e1d801118fb37ae752d2c43d77cc
Oladele F <19507596+faradele@users.noreply.github.com>
2022-07-04 22:07:15 +0100

    feat: production build for nova 4 upgrade

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,7 @@
     .setPublicPath('dist')
     .js('resources/js/field.js', 'js')
     .vue({ version: 3 })
-    // .sass('resources/css/field.scss', 'css')
+    // .sass('resources/sass/field.scss', 'css')
     .alias({
       '@': path.join(__dirname, 'resources/js/'),
     })
